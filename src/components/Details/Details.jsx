64334e928897b544ee765eecfb5d8441f9a03b8c
VJS <minsung_96@naver.com>
2021-09-19 23:54:20 +0900

    Rating

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Box, Card, Buttons, CardMedia, CardContent, CardActions, Chip, Typography} from '@material-ui/core'
+import { Box, Card, Button, CardMedia, CardContent, CardActions, Chip, Typography} from '@material-ui/core'
 import LocationOnIcon from '@material-ui/icons/LocationOn'
-import Phone from '@material-ui/icons/Phone'
+import PhoneIcon from '@material-ui/icons/Phone'
 import Rating from '@material-ui/lab/Rating'
 
 import useStyles from './styles'
@@ -19,6 +19,57 @@ const Details = ({ place }) => {
                 <Typography gutterBottom variant="h5">
                     {place.name}
                 </Typography>
+                <Box display="flex" justifyContent="space-between">
+                <Rating value={Number(place.rating)} readOnly /> 
+                    <Typography gutterBottom variant="subtitle1">
+                        {place.num_reviews}
+                    </Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between"> 
+                    <Typography  variant="subtitle1">
+                        Price
+                    </Typography>
+                    <Typography gutterBottom variant="subtitle1">
+                        {place.price}
+                    </Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between"> 
+                    <Typography  variant="subtitle1">
+                        Ranking
+                    </Typography>
+                    <Typography gutterBottom variant="subtitle1">
+                        {place.ranking}
+                    </Typography>
+                </Box>
+                {place?.awards?.map((award) => (
+                    <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+                        <img src={award.images.small} alt={award.display_name} />
+                        <Typography variant="subtitle2" color="textSecondary">
+                            {award.display_name}
+                        </Typography>
+                    </Box>
+                ))}
+                {place?.cuisine?.map(({ name }) => (
+                    <Chip key={name} size="small" label={name} className={classes.chip}/>
+                ))}
+                {place?.address && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.subtitle}>
+                        <LocationOnIcon /> {place.address}
+                    </Typography>
+                )}
+                {place?.phone && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.spacing}>
+                        <PhoneIcon /> {place.phone}
+                    </Typography>
+                )}
+                <CardActions>
+                    <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+                        Trip Advisor
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+                        Move to Website
+                    </Button>
+                </CardActions>
             </CardContent>
         </Card>
     )
